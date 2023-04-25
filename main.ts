3940e0471871cefba8119f4271a390544d970788
Ben Hocquet <78877792+Benpitt@users.noreply.github.com>
2023-04-25 19:27:54 -0400

    added sensor detection , radio

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+let sensor = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+if (sensor > 400 && sensor < 2) {
+    radio.sendString("" + (sensor))
+    basic.showIcon(IconNames.No)
+} else {
+    basic.showIcon(IconNames.Yes)
+}
+basic.forever(function () {
+	
+})
